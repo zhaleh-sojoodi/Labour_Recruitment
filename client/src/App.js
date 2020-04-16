235f078ab9c02cfc55999bc6df0719fd56ccff4c
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-15 20:25:55 -0700

    Add Client views and layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-    return <h1>Hello</h1>
+import Home from './views/Home';
+import Dashboard from './views/Dashboard';
+import AddJob from './views/client/AddJob';
+import ClientProfile from './views/client/ClientProfile';
+import PageNotFound from './views/PageNotFound';
+
+const App = () => {
+    return (
+    <>
+    <Router>
+    <Switch>
+        <Route path='/' exact render={props => <Home {...props} />} />
+        <Route path='/dashboard' exact render={props => <Dashboard {...props} />} />
+
+        {/* Client Views */}
+        <Route path='/addjob' exact render={props => <AddJob {...props} />} />
+        <Route path='/profile/client' exact render={props => <ClientProfile {...props} />} />
+
+        <Route component={PageNotFound} />
+    </Switch>
+    </Router>
+    </>
+    );
 }
 
 export default App;
