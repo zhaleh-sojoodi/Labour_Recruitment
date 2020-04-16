1c11cf940491e7a5e6b1092c6ee4c85a2a8844a9
Jaleh Sojoodi <jaleh36@gmail.com>
2020-04-16 00:51:40 -0700

    #17 Created client profile layout

--- client/src/views/client/ClientProfile.js
+++ client/src/views/client/ClientProfile.js
@@ -6,9 +6,9 @@ import TopNav from '../../components/TopNav';
 import '../../css/styles.css';
 import '../../css/extras.min.css';
 
+
 const ClientProfile = (props) => {
     return (
-    <>
     <div className="container-fluid">
     <div className="row">
       {/* Side Navigation */}
@@ -26,15 +26,62 @@ const ClientProfile = (props) => {
             <h3 className="page-title">Client Profile</h3>
           </div>
           </div>
-
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex error maxime repudiandae ipsam commodi necessitatibus reiciendis beatae deserunt placeat at veniam possimus velit, laboriosam ducimus, amet facere? Voluptatem, cum sed?</p>
-
+           <div class="row">
+            <div class="col-lg-4">
+              <div class="mb-4 pt-3 card card-small">
+                <div class="border-bottom text-center card-header">
+                  <h4 class="mb-0">Sierra Brooks</h4>
+                  <span class="m-5"><i class="fas fa-star mr-2"></i>4.91 (18 ratings)</span>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="p-4 list-group-item">
+                    <strong class="text-muted d-block mb-2">Description</strong>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eaque, quidem, commodi soluta qui quae minima obcaecati quod dolorum sint alias, possimus illum assumenda eligendi cumque?</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-8">
+              <div class="mb-4 card card-small">
+                <div class="border-bottom card-header d-flex justify-content-between">
+                  <h6 class="m-0">Account Details</h6>
+                  <span><i class="far fa-edit"></i></span>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="p-3 list-group-item">
+                    <table>
+                      <tr>
+                        <td class="pl-4 pr-4 pb-3">Email:</td>
+                        <td class="pl-4 pr-4 pb-3">info@construction.ca</td>
+                      </tr>
+                      <tr>
+                        <td  class="pl-4 pr-4 pb-3">Telephone No.:</td>
+                        <td class="pl-4 pr-4 pb-3">(604)778-8888</td>
+                      </tr>
+                      <tr>
+                        <td  class="pl-4 pr-4 pb-3">Address:</td>
+                        <td class="pl-4 pr-4 pb-3">1234 Burrad Street, Vancouver BC</td>
+                      </tr>
+                    </table>
+                  </li>
+                  <li class="p-3 pb-4 list-group-item">
+                    <div class="row">
+                      <div class="col">
+                        <span class="pl-4 pr-4 pb-3">Rate The Company</span>
+                        <span class="pl-4 pr-4 pb-3"><i class="fas fa-star mr-2"></i><i class="fas fa-star mr-2"></i><i class="fas fa-star mr-2"></i><i class="fas fa-star mr-2"></i><i class="fas fa-star mr-2"></i></span>
+                      </div>
+                    </div>
+                  </li>
+                </ul> 
+              </div> 
+            </div>
+          </div>
         </div>
       </main>
     </div>
     </div>
-    </>
-    )
+
+  )
 }
 
 export default ClientProfile;
