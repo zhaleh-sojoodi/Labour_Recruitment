6400560ea771a41f1fa92376c5aedf58fc62999e
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-17 23:27:01 -0700

    Edit comments

--- client/public/scripts/data-table.js
+++ client/public/scripts/data-table.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($) {
     'use strict';
 
+    // Client Dashboard Table
     if ($("table.client-overview-table").length) {
         $(document).ready(function() {
             $('table.client-overview-table').DataTable({
                 columnDefs: [ {
-                    'targets': [0, 1,2,3,4,5,6], /* column index */
-                    'orderable': false, /* true or false */
+                    'targets': [0,1,2,3,4,5,6],
+                    'orderable': false
                 }],
                 aaSorting: []
             });
