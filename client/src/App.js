9d58b72224de5181bfcf12684dddc24280bcdecc
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-18 05:27:02 -0700

    Register new Register page routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './views/Home';
-import Dashboard from './views/Dashboard';
-import AddJob from './views/client/AddJob';
-import ClientProfile from './views/client/ClientProfile';
-import LabourerProfile from './views/labourer/LabourerProfile';
 import RegisterStart from './views/RegisterStart';
 import PageNotFound from './views/PageNotFound';
+import Dashboard from './views/Dashboard';
+
+import RegisterClient from './views/client/RegisterClient';
+import AddJob from './views/client/AddJob';
+import ClientProfile from './views/client/ClientProfile';
+
+import RegisterLabourer from './views/labourer/RegisterLabourer';
+import LabourerProfile from './views/labourer/LabourerProfile';
+
 
 const App = () => {
     return (
@@ -16,13 +21,15 @@ const App = () => {
     <Switch>
         <Route path='/' exact render={props => <Home {...props} />} />
         <Route path='/dashboard' exact render={props => <Dashboard {...props} />} />
-        <Route path='/register/start' exact render={props => <RegisterStart {...props} />} />
+        <Route path='/register' exact render={props => <RegisterStart {...props} />} />
 
         {/* Client Views */}
+        <Route path='/register/client' exact render={props => <RegisterClient {...props} />} />
         <Route path='/addjob' exact render={props => <AddJob {...props} />} />
         <Route path='/profile/client' exact render={props => <ClientProfile {...props} />} />
 
         {/* Labourer Views */}
+        <Route path='/register/labourer' exact render={props => <RegisterLabourer {...props} />} />
         <Route path='/profile/labourer' exact render={props => <LabourerProfile {...props} />} />
 
         <Route component={PageNotFound} />
