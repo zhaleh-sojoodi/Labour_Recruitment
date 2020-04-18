b24fe048560cda391d75af4a1030ce1a7b40f496
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-17 22:26:15 -0700

    Remove unneeded CSS imports

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,6 @@ import ClientProfile from './views/client/ClientProfile';
 import LabourerProfile from './views/labourer/LabourerProfile';
 import PageNotFound from './views/PageNotFound';
 
-import './css/styles.css';
-import './css/extras.min.css';
-import './css/custom.css';
-
 const App = () => {
     return (
     <>
