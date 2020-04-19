3d23d1921763099b1aa20d549ca8a6e10e19ff58
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-19 09:00:20 -0700

    Add routes for Login, Incident, JobDetail and IncidentDetail

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './views/Home';
+import Login from './views/Login';
 import RegisterStart from './views/RegisterStart';
 import PageNotFound from './views/PageNotFound';
+
+// Shared Components
 import Dashboard from './views/Dashboard';
+import Incidents from './views/Indicents';
+import JobDetail from './views/JobDetail';
+import IncidentDetail from './views/IncidentDetail';
 
 import RegisterClient from './views/client/RegisterClient';
 import AddJob from './views/client/AddJob';
+// Client Components
+import ClientRegister from './views/client/ClientRegister';
 import ClientProfile from './views/client/ClientProfile';
-import RegisterLabourer from './views/labourer/RegisterLabourer';
+import ClientAddJob from './views/client/ClientAddJob';
+
+// Labourer Components
+import LabourerRegister from './views/labourer/LabourerRegister';
 import LabourerProfile from './views/labourer/LabourerProfile';
-
-
 
 const App = () => {
     return (
@@ -20,17 +29,22 @@ const App = () => {
     <Router>
     <Switch>
         <Route path='/' exact render={props => <Home {...props} />} />
-        <Route path='/dashboard' exact render={props => <Dashboard {...props} />} />
+        <Route path='/login' exact render={props => <Login {...props} />} />
         <Route path='/register' exact render={props => <RegisterStart {...props} />} />
 
+        {/* Shared Views */}
+        <Route path='/dashboard' exact render={props => <Dashboard {...props} />} />
+        <Route path='/incidents' exact render={props => <Incidents {...props} />} />
+        <Route path='/job' exact render={props => <JobDetail {...props} />} />
+        <Route path='/incident' exact render={props => <IncidentDetail {...props} />} />
       
         {/* Client Views */}
-        <Route path='/register/client' exact render={props => <RegisterClient {...props} />} />
-        <Route path='/addjob' exact render={props => <AddJob {...props} />} />
+        <Route path='/register/client' exact render={props => <ClientRegister {...props} />} />
         <Route path='/profile/client' exact render={props => <ClientProfile {...props} />} />
+        <Route path='/addjob' exact render={props => <ClientAddJob {...props} />} />
 
         {/* Labourer Views */}
-        <Route path='/register/labourer' exact render={props => <RegisterLabourer {...props} />} />
+        <Route path='/register/labourer' exact render={props => <LabourerRegister {...props} />} />
         <Route path='/profile/labourer' exact render={props => <LabourerProfile {...props} />} />
 
         <Route component={PageNotFound} />
