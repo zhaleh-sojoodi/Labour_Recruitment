215c25486a7fbcc0022f0bac32994ebd88ba5f52
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-19 02:48:46 -0700

    Comment initialization of client dashboard datatable

--- client/public/scripts/data-table.js
+++ client/public/scripts/data-table.js
@@ -2,15 +2,15 @@ jQuery(document).ready(function($) {
     'use strict';
 
     // Client Dashboard Table
-    if ($("table.client-overview-table").length) {
-        $(document).ready(function() {
-            $('table.client-overview-table').DataTable({
-                columnDefs: [ {
-                    'targets': [0,1,2,3,4,5,6],
-                    'orderable': false
-                }],
-                aaSorting: []
-            });
-        });
-    }
+    // if ($("table.client-overview-table").length) {
+    //     $(document).ready(function() {
+    //         $('table.client-overview-table').DataTable({
+    //             columnDefs: [ {
+    //                 'targets': [0,1,2,3,4,5,6],
+    //                 'orderable': false
+    //             }],
+    //             aaSorting: []
+    //         });
+    //     });
+    // }
 });
