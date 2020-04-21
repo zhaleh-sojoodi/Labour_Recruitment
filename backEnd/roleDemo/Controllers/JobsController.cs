f69d9996e32ba90945fb03d0f3fc8758b5034aa5
neilquizon <neil.quizon@gmail.com>
2020-04-21 13:12:30 -0700

    created ClientsController and tested getClients in postman #8

---
+++ backEnd/roleDemo/Controllers/JobsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using labourRecruitment.Models.LabourRecruitment;
+
+namespace labourRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Jobs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Job>>> GetJob()
+        {
+            return await _context.Job.ToListAsync();
+        }
+
+        // GET: api/Jobs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            var job = await _context.Job.FindAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+        // PUT: api/Jobs/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, Job job)
+        {
+            if (id != job.JobId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JobExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Jobs
+        [HttpPost]
+        public async Task<ActionResult<Job>> PostJob(Job job)
+        {
+            _context.Job.Add(job);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJob", new { id = job.JobId }, job);
+        }
+
+        // DELETE: api/Jobs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Job>> DeleteJob(int id)
+        {
+            var job = await _context.Job.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Job.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return job;
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Job.Any(e => e.JobId == id);
+        }
+    }
+}
