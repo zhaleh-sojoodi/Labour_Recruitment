13c9e69f219a50010028ef82880f9ece3b19bbc3
neilquizon <neil.quizon@gmail.com>
2020-04-21 23:20:40 -0700

    added ClientProfileController #17

---
+++ backEnd/roleDemo/Controllers/ClientProfileController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using labourRecruitment.Models.LabourRecruitment;
+using labourRecruitment.Repositories;
+using labourRecruitment.ViewModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace labourRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientProfileController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<ClientProfileVM>> GetLClientProfile(int id)
+        {
+            return  new ClientProfileVMRepo(_context).GetClient(id);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> PutClientProfile(ClientProfileVM clientProfile)
+        {
+            var lp = _context.Client.SingleOrDefault(l => l.ClientId == clientProfile.Client.ClientId);
+
+            if (lp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                lp.ClientName = clientProfile.Client.ClientName;               
+             
+                lp.ClientEmail = clientProfile.Client.ClientEmail;
+             }
+
+
+            //_context.AvailabilityLabourer.RemoveRange(_context.AvailabilityLabourer.Where(al => al.LabourerId == labourerProfile.Labourer.LabourerId));
+
+            //foreach (string day in labourerProfile.Availabilities.Select(av=> av.AvailabilityDay))
+            //{
+            //    Availability availability = _context.Availability.Where(a => a.AvailabilityDay == day).FirstOrDefault();
+            //    if (availability != null)
+            //    {
+            //        AvailabilityLabourer availabilityLabourer = new AvailabilityLabourer
+            //        {
+            //            AvailabilityId = availability.AvailabilityId,
+            //            LabourerId = labourerProfile.Labourer.LabourerId
+            //        };
+            //        _context.AvailabilityLabourer.Add(availabilityLabourer);
+            //        _context.SaveChanges();
+            //    }
+            //    else
+            //    {
+            //        return NotFound();
+            //    }
+
+            //}
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                throw;
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/
+
+        [HttpDelete]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> DeleteClientProfile(int id)
+        {
+
+
+            var lb = await _context.Client.FindAsync(id);
+            if (lb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Client.Remove(lb);
+            await _context.SaveChangesAsync();
+            return new ObjectResult(lb);
+
+        }
+
+
+
+
+    }
+}
---
+++ backEnd/roleDemo/Repositories/ClientProfileVMRepo.cs
@@ -0,0 +1,59 @@
+﻿using labourRecruitment.Models.LabourRecruitment;
+using labourRecruitment.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labourRecruitment.Repositories
+{
+    public class ClientProfileVMRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientProfileVMRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //public IEnumerable<LabourerProfileVM> GetAll()
+        //{
+        //    return _context.Labourer.Select(l => new LabourerProfileVM()
+        //    {
+        //        Labourer = l,
+        //        Availabilities = l.AvailabilityLabourer,
+        //        AverageSafety = l.LabourerSafetyRating
+        //    });
+        //}
+
+        public ClientProfileVM GetClient(int clientID)
+        {
+            Client Client = _context.Client.FirstOrDefault(l => l.ClientId == clientID);
+            //  List<double> a = new List<double>();
+            //    a.Add(3.4d);
+            // var a =  _context.LabourerSafetyRating.Where(lsr => lsr.LabourerId == labourerID).Select(l => new List<double>().Add ( l.SafetyRating)).ToList();
+            // var b = a;
+
+            //  var L = _context.LabourerSafetyRating.Where(lsr => lsr.LabourerId == labourerID).ToList();
+
+            //var sum = 0.0d;
+            //foreach (LabourerSafetyRating l in L)
+            //{
+            //    sum = sum + (double)l.Rating;
+            //}
+
+            ////var avgerageSafety = _context.LabourerSafetyRating
+            ////      .Where(l => l.LabourerId == labourerID && l.Rating != null).Average(av => av.Rating);
+            
+            var avgerageQuality = _context.ClientQualityRating
+                 .Where(l => l.ClientId == clientID && l.Rating != null).Average(av => av.Rating);
+            ClientProfileVM lp = new ClientProfileVM()
+            {
+                Client = Client,
+                AverageRating = (double)avgerageQuality
+                
+            };
+            return lp;
+        }
+    }
+}
