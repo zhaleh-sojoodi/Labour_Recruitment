95265da05b7d96a2dade87a16546127764655891
mingwang168 <mingwang0904@gmail.com>
2020-04-23 13:23:13 -0700

    close #101 API : PostClientQualityRating

---
+++ backEnd/roleDemo/Controllers/JobHistoryController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using labourRecruitment.Models.LabourRecruitment;
+using labourRecruitment.ViewModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace labourRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobHistoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobHistoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LabourerID
+        [HttpGet("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<IEnumerable<JobHistoryVM>>> GetJobHistorys(int id)
+        {
+
+            return await _context.JobLabourer.Where(jl => jl.LabourerId == id).Select(ojl => new JobHistoryVM()
+            {
+                Job = ojl.Job,
+                report = ojl.ClientQualityRating != null ? "Complete" : "Required"
+            }).ToListAsync();
+
+        }
+
+
+        // GET: api/LabourerID
+        [HttpPut]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> PutClientQualityRating([FromBody] JobLabourerRating jlr)
+        {
+            var jobLabourer = _context.JobLabourer.SingleOrDefault(jl => jl.LabourerId == jlr.LabourerId && jl.JobId == jlr.JobId);
+
+            if (jobLabourer != null) 
+            {
+                jobLabourer.ClientQualityRating = jlr.ClientQualityRating;
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                throw;
+            }
+            return new ObjectResult(jobLabourer);
+        }
+    }
+
+    public class JobLabourerRating
+    {
+        public int? JobId { get; set; }
+        public int? LabourerId { get; set; }
+        public double? ClientQualityRating { get; set; }
+    }
+}
---
+++ backEnd/roleDemo/ViewModels/JobHistoryVM.cs
@@ -0,0 +1,14 @@
+﻿using labourRecruitment.Models.LabourRecruitment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labourRecruitment.ViewModels
+{
+    public class JobHistoryVM
+    {
+        public Job Job { get; set; }
+        public string report { get; set; }
+    }
+}
