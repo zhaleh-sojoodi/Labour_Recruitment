d47a982b4ed1e4b10f1784649ca77d4bddbe46ab
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-25 04:26:14 -0700

    Simplify logic

--- client/src/utils/DayCalculator.js
+++ client/src/utils/DayCalculator.js
@@ -1,13 +1,9 @@
 exports.difference = (start, end) => {
-    if(start === end) {
-        return 1;
+    if(start.getTime() === end.getTime()) {
+        return "1 day";
     }
     const day = 24 * 60 * 60 * 1000;
-    let a = start.split("-");
-    let b = end.split("-");
-    let d1 = new Date(a[0], a[1]-1, a[2]);
-    let d2 = new Date(b[0], b[1]-1, b[2]);
-    let diff = Math.round(Math.abs((d1 - d2) / day));
+    let diff = Math.round(Math.abs((start - end) / day));
     return `${diff} days`;
 }
 
