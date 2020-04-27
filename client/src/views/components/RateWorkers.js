7a7b325231ecd1e54d899a8e67afff3dcac24f9d
Jaleh Sojoodi <jaleh36@gmail.com>
2020-04-26 23:22:14 -0700

    #143 Fetch PutLabourerSafetyRating

--- client/src/views/components/RateWorkers.js
+++ client/src/views/components/RateWorkers.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Ratings from 'react-ratings-declarative';
+import {forceLogout} from '../../utils/Auth'
 
 const BASE_URL = "http://localhost:5001/api";
 const RateWorkers = (props) => {
@@ -7,6 +8,9 @@ const RateWorkers = (props) => {
 
     const changeSafetyRating = async(newRating) => {
         let token = sessionStorage.getItem("auth_token")
+        if (token == null) {
+            forceLogout()
+        }
         try{
             const response = await fetch(BASE_URL + '/JobHistory/LabourerSafety', {
                 method : 'PUT',
