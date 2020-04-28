3373bf0e5d8d057e0af0f12a8111fdefd719f7dd
Jaleh Sojoodi <jaleh36@gmail.com>
2020-04-28 16:24:29 -0700

    Added GetIncidentType

---
+++ backEnd/roleDemo/Controllers/IncidentTypeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using labourRecruitment.Models.LabourRecruitment;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace labourRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentTypeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IncidentTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<IncidentType>>> GetIncidentType()
+        {
+            return await _context.IncidentType.ToListAsync();
+        }
+    }
+}
