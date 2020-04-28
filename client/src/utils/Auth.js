2a73510ac02ee712094fdef3ad3f5cde3e36c946
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-28 07:26:40 -0700

    Add token validation check and function for setting user data at login

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -1,47 +1,75 @@
+const AUTH_TOKEN = "auth_token";
+const USER_NAME = "user_name";
+const USER_ID = "user_id";
+const USER_ROLE = "user_role";
+const TOKEN_EXP = "token_exp";
+
+exports.setSessionData = (data, history) => {
+    sessionStorage.setItem(AUTH_TOKEN, data.token);
+    sessionStorage.setItem(TOKEN_EXP, this.setTokenExpiration());
+    sessionStorage.setItem(USER_ROLE, data.role);
+    sessionStorage.setItem(USER_ID, data.id);
+    sessionStorage.setItem(USER_NAME, data.name);
+    history.push("/dashboard");
+}
+
 exports.authenticateUser = _ => {
     let authenticated =
-    sessionStorage.getItem("auth_token") &&
-    sessionStorage.getItem("auth_token") !== "" &&
-    sessionStorage.getItem("user_role") &&
+    sessionStorage.getItem(AUTH_TOKEN) &&
+    sessionStorage.getItem(AUTH_TOKEN) !== "" &&
+    sessionStorage.getItem(USER_ROLE) &&
     (
-        sessionStorage.getItem("user_role") === "Admin" ||
-        sessionStorage.getItem("user_role") === "Client" ||
-        sessionStorage.getItem("user_role") === "Labourer"
+        sessionStorage.getItem(USER_ROLE) === "Admin" ||
+        sessionStorage.getItem(USER_ROLE) === "Client" ||
+        sessionStorage.getItem(USER_ROLE) === "Labourer"
     ) &&
-    sessionStorage.getItem("user_name") &&
-    sessionStorage.getItem("user_id") ? true : false;
+    sessionStorage.getItem(USER_NAME) &&
+    sessionStorage.getItem(USER_ID) ? true : false;
     if(!authenticated) {
         sessionStorage.clear();
     }
     return authenticated;
 }
 
-exports.getToken = _ => {
-    return sessionStorage.getItem("auth_token");
+exports.validateToken = _ => {
+    return Date.now() > this.getTokenExpirationTime() ? false : true;
 }
 
-exports.getID = _ => {
-    return sessionStorage.getItem("user_id");
+exports.setTokenExpiration = _ => {
+    const HOUR = 1000 * 60 * 60;
+    return Date.now() + (HOUR * 2);
 }
 
-exports.getRole = _ => {
-    return sessionStorage.getItem("user_role");
-}
-
-exports.getName = _ => {
-    return sessionStorage.getItem("user_name");
+exports.getTokenExpirationTime = _ => {
+    return sessionStorage.getItem(TOKEN_EXP);
 }
 
 exports.authenticateClient = _ => {
-    return sessionStorage.getItem("user_role") === "Client" ? true : false;
+    return sessionStorage.getItem(USER_ROLE) === "Client" ? true : false;
 }
 
 exports.authenticateLabourer = _ => {
-    return sessionStorage.getItem("user_role") === "Labourer" ? true : false;
+    return sessionStorage.getItem(USER_ROLE) === "Labourer" ? true : false;
 }
 
 exports.authenticateAdmin = _ => {
-    return sessionStorage.getItem("user_role") === "Admin" ? true : false;
+    return sessionStorage.getItem(USER_ROLE) === "Admin" ? true : false;
+}
+
+exports.getToken = _ => {
+    return sessionStorage.getItem(AUTH_TOKEN);
+}
+
+exports.getID = _ => {
+    return sessionStorage.getItem(USER_ID);
+}
+
+exports.getRole = _ => {
+    return sessionStorage.getItem(USER_ROLE);
+}
+
+exports.getName = _ => {
+    return sessionStorage.getItem(USER_NAME);
 }
 
 exports.forceLogout = _ => {
