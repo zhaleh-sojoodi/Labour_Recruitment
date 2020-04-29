408ac0c3488827b403026a6c83b551d1443277f3
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-29 06:09:40 -0700

    Add stats to dashboard

--- client/src/views/admin/AdminDashboard.js
+++ client/src/views/admin/AdminDashboard.js
@@ -56,6 +56,65 @@ const AdminDashboard = (props) => {
     </div>
     </div>
 
+    {/* Stats */}
+    <div className="row">
+        <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+	        <div className="card">
+	        <div className="card-body">
+                <div className="d-inline-block">
+                    <h5 className="text-muted">Active Jobs</h5>
+                    <h2 className="mb-0">{jobs && jobs.length} jobs</h2>
+                </div>
+                <div className="float-right icon-circle-medium icon-box-lg bg-info-light mt-1">
+                    <i className="material-icons text-info">work</i>
+                </div>
+	        </div>
+	        </div>
+	     </div>
+
+        <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+	        <div className="card">
+	        <div className="card-body">
+                <div className="d-inline-block">
+                    <h5 className="text-muted">Total Clients</h5>
+                    <h2 className="mb-0">3 clients</h2>
+                </div>
+                <div className="float-right icon-circle-medium icon-box-lg bg-success-light mt-1">
+                    <i className="material-icons text-success">business</i>
+                </div>
+	        </div>
+	        </div>
+	    </div>
+
+        <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+	        <div className="card">
+	        <div className="card-body">
+                <div className="d-inline-block">
+                    <h5 className="text-muted">Total Labourers</h5>
+                    <h2 className="mb-0">3 labourers</h2>
+                </div>
+                <div className="float-right icon-circle-medium icon-box-lg bg-brand-light mt-1">
+                    <i className="material-icons text-brand">emoji_people</i>
+                </div>
+	        </div>
+	        </div>
+	    </div>
+
+        <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+	        <div className="card">
+	        <div className="card-body">
+                <div className="d-inline-block">
+                    <h5 className="text-muted">Incident Reports</h5>
+                    <h2 className="mb-0">1 report(s)</h2>
+                </div>
+                <div className="float-right icon-circle-medium icon-box-lg bg-secondary-light mt-1">
+                    <i className="material-icons text-secondary">warning</i>
+                </div>
+	        </div>
+	        </div>
+	    </div>
+    </div>
+
     {/* Active Jobs Table */}
     <div className="row">
     <div className="col">
