956f97a0cc4f6963046d0350e4825f471c6ed27e
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-29 01:41:50 -0700

    Complete layout and fetch all jobs
    Closes #21

---
+++ client/src/views/admin/AdminJobs.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+import * as Auth from '../../utils/Auth';
+import * as DataSanitizer from '../../utils/DataSanitizer';
+
+import Table from '../components/Table';
+import TopNav from '../components/TopNav';
+import SideNav from '../components/SideNav';
+import Footer from '../components/Footer';
+
+import { ADMIN_JOBS_TABLE_COLUMNS   } from '../../utils/TableColumns';
+const BASE_URL = "http://localhost:5001/api";
+
+const AdminJobs = (props) => {
+
+    const [jobs, setJobs] = useState();
+
+    const fetchJobs = async() => {
+        try {
+            let response = await fetch(BASE_URL + "/Job/GetAllJobs", {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${Auth.getToken()}`
+                }
+            });
+
+            let data = await response.json();
+            if(data.length) {
+                setJobs(DataSanitizer.cleanAdminJobs(data));
+                console.log(data);
+            }
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchJobs();
+    }, [])
+
+    return (
+    <div className="dashboard-main-wrapper">
+        <TopNav />
+        <SideNav />
+    
+        <div className="dashboard-wrapper">
+            <div className="container-fluid dashboard-content">
+                {/* Page Header */}
+                <div className="page-header">
+                <h2 className="pageheader-title">
+                    All Jobs
+                </h2>
+                <div className="page-breadcrumb">
+                <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/dashboard" className="breadcrumb-link">Home</Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                        All Jobs
+                    </li>
+                </ol>
+                </nav>
+                </div>
+                </div>
+
+                {/* All Jobs Table */}
+                <div className="row">
+                <div className="col">
+                <div className="card">
+                <h5 className="card-header">All Jobs</h5>
+                <div className="card-body">
+                    { jobs ?
+                    <Table
+                        columns={ADMIN_JOBS_TABLE_COLUMNS}
+                        data={jobs}
+                        {...props}
+                    />
+                    :
+                    <p className="lead">No jobs to display.</p>
+                    }
+                </div>
+                </div>
+                </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    </div>
+    );
+}
+
+export default AdminJobs;
