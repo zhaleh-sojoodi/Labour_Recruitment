65e55e1de706c4f9cdf885e3859b49ae475c38e0
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-29 01:29:28 -0700

    Fetch all active jobs (in progress only)

--- client/src/views/admin/AdminDashboard.js
+++ client/src/views/admin/AdminDashboard.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-const AdminDashboard = ({name}) => {
+import * as Auth from '../../utils/Auth';
+import * as DataSanitizer from '../../utils/DataSanitizer';
+import Table from '../components/Table';
+import { ADMIN_DASHBOARD_JOBS_TABLE_COLUMNS   } from '../../utils/TableColumns';
+
+const BASE_URL = "http://localhost:5001/api";
+
+const AdminDashboard = (props) => {
+
+    const [jobs, setJobs] = useState();
+
+    const fetchJobs = async() => {
+        try {
+            let response = await fetch(BASE_URL + "/Job/GetAllActiveJobs", {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${Auth.getToken()}`
+                }
+            });
+
+            let data = await response.json();
+            if(data.length) {
+                setJobs(DataSanitizer.cleanAdminJobs(data));
+                console.log(data);
+            }
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchJobs();
+    }, [])
+
     return (
     <>
     {/* Page Header */}
     <div className="page-header">
     <h2 className="pageheader-title">
-        Welcome back, {name}!
+        Welcome back, {props.name}!
     </h2>
     <div className="page-breadcrumb">
     <nav aria-label="breadcrumb">
@@ -20,6 +53,26 @@ const AdminDashboard = ({name}) => {
         </li>
     </ol>
     </nav>
+    </div>
+    </div>
+
+    {/* Active Jobs Table */}
+    <div className="row">
+    <div className="col">
+    <div className="card">
+    <h5 className="card-header">Active Jobs</h5>
+    <div className="card-body">
+        { jobs ?
+        <Table
+            columns={ADMIN_DASHBOARD_JOBS_TABLE_COLUMNS}
+            data={jobs}
+            {...props}
+        />
+        :
+        <p className="lead">No jobs to display.</p>
+        }
+    </div>
+    </div>
     </div>
     </div>
     </>
