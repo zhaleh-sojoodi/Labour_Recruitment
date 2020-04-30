0fc7d6fbf9dc6a3d8bf5e69172f59e77a67d2910
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-04-29 22:09:47 -0700

    Remove unneeded CSS files

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// General CSS
+// CSS Files
 import './assets/other/bootstrap/css/bootstrap.min.css';
 import './assets/fonts/circular-std/style.css';
 import './assets/css/style.css';
-import './assets/other/bootstrap-select/css/bootstrap-select.css';
-import './assets/other/datatables/css/dataTables.bootstrap4.css';
-import './assets/other/datatables/css/select.bootstrap4.css';
 
 ReactDOM.render(
   <App />, document.getElementById('root')
