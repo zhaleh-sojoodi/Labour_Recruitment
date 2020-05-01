e8e1b43ead8e9a3ff1f4d16bc77e87bfa75df15f
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-01 05:20:09 -0700

    Fetch labourer list

--- client/src/views/client/ClientLabourerAttendance.js
+++ client/src/views/client/ClientLabourerAttendance.js
@@ -4,13 +4,41 @@ import { Link } from 'react-router-dom';
 import TopNav from '../components/TopNav';
 import SideNav from '../components/SideNav';
 import Footer from '../components/Footer';
+import Table from '../components/Table';
 
 import * as Auth from '../../utils/Auth';
 import * as DataSanitizer from '../../utils/DataSanitizer';
 
+import { ATTENDANCE_RATINGS_TABLE_COLUMNS } from '../../utils/TableColumns';
 const BASE_URL = "http://localhost:5001/api";
 
 const ClientLabourerAttendance = (props) => {
+
+    const [list, setList] = useState();
+
+    const getList = async(id, date) => {
+        try {
+            const response = await fetch(BASE_URL + `/LabourerAttendance/data?jobId=${id}&date=${date}`, {
+                method : 'GET',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Auth.getToken()}`
+                }
+            })
+            let data = await response.json();
+            setList(DataSanitizer.cleanAttendanceRatingsData(data));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() =>{
+        if(props.match.params.id && props.match.params.date) {
+            let date = props.match.params.date + "T00:00:00"
+            getList(props.match.params.id, date);
+        }
+    }, [])
 
     return (
     <div className="dashboard-main-wrapper">
@@ -25,16 +53,23 @@ const ClientLabourerAttendance = (props) => {
                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div className="page-header">
                     <h2 className="pageheader-title">
-                        Labourer Attendance for {DataSanitizer.formatDateString(props.match.params.date)}
+                        {`Labourer Attendance for ${DataSanitizer.formatDateString(props.match.params.date)}`}
                     </h2>
                     <hr />
+                    <h5 onClick={() => props.history.goBack()} className="text-muted" style={{cursor:'pointer'}}>&larr; Go back</h5>
                 </div>
                 </div>
                 </div>
 
                <div className="card">
                    <div className="card-body">
-                       
+                   { list &&
+                        <Table
+                            columns={ATTENDANCE_RATINGS_TABLE_COLUMNS}
+                            data={list}
+                            {...props}
+                        />
+                    } 
                    </div>
                </div>
                 
