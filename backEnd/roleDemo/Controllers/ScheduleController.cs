dfaff7355953ec0e0b8c1be267c411208a75237e
Jaleh Sojoodi <jaleh36@gmail.com>
2020-05-01 22:52:29 -0700

    Moved Schedule Function to SheduleRepo Class

--- backEnd/roleDemo/Controllers/ScheduleController.cs
+++ backEnd/roleDemo/Controllers/ScheduleController.cs
@@ -18,56 +18,7 @@ namespace labourRecruitment.Controllers
             _context = context;
         }
 
-        [HttpPut("{jobId}")]
-        public IActionResult AddLabourersToFirstSchedule(int jobId)
-        {
-            var jobSelected = _context.Job.Where(j => j.JobId == jobId).FirstOrDefault();
-            var jobSkillSelected = _context.Job.Where(j => j.JobId == jobId).Select(j => j.JobSkill).FirstOrDefault();
-            ScheduleRepo scheduleRepo = new ScheduleRepo(_context);
-            var duration = (jobSelected.EndDate - jobSelected.StartDate).TotalDays;
-            DateTime eDate;
-            if (duration <= 14)
-            {
-               eDate = jobSelected.EndDate;
-            } 
-            else
-            {
-                eDate = jobSelected.StartDate.AddDays(ScheduleRepo.CalculateLastDay(jobSelected.StartDate));
-            }
-
-            HighestRatedLabourers rated = new HighestRatedLabourers(_context);
-            foreach (JobSkill js in jobSkillSelected)
-            {
-                var ratedLabourers = rated.GetHighestRatedLabourersAsync(js.SkillId).ToList();
-                List<Labourer> labourers = new List<Labourer>();
-                labourers.AddRange(ratedLabourers.GetRange(0, js.NumberNeeded));
-
-                foreach (Labourer labourer in labourers)
-                {
-                    _context.Labourer.FirstOrDefault(l => l.LabourerId == labourer.LabourerId).IsAvailable = false;
-
-                    _context.JobLabourer.Add(new JobLabourer
-                    {
-                        JobId = jobId,
-                        SkillId = js.SkillId,
-                        LabourerId = labourer.LabourerId,
-                        LabourerSafetyRating = 5,
-                        SafetyMeetingCompleted = false,
-                        ClientQualityRating = 0,
-                        StartDay = jobSelected.StartDate,
-                        EndDay = eDate,
-                        Duration = ScheduleRepo.GetBusinessDays(jobSelected.StartDate, eDate)
-                    });
-                    scheduleRepo.PopulateLabourerAttendance(jobId, labourer.LabourerId, jobSelected.StartDate, eDate);
-                }
-                _context.SaveChanges();
-
-            }
-
-            return new ObjectResult(jobId);
-        }
-
-        
+      
     }
 
 }
