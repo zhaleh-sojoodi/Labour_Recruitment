736d87c3669b9de68b0ac51896b80832217299a0
Jaleh Sojoodi <jaleh36@gmail.com>
2020-05-03 22:59:05 -0700

    Updated GetHighestRatedLabourers to check isAvilable field

--- backEnd/roleDemo/Repositories/HighestRatedRepo.cs
+++ backEnd/roleDemo/Repositories/HighestRatedRepo.cs
@@ -18,11 +18,8 @@ namespace labourRecruitment.Repositories
 
         public IEnumerable<Labourer> GetHighestRatedLabourers(int id)
         {
-            List<Labourer> availableLabourers = _context.LabourerSkill.Where(ls => ls.SkillId == id).Select(ols => ols.Labourer
-                       ).ToList();
-            //List<Labourer> labourerScheduled = _context.LabourerAttendance.Where(l => l.Date > today).Select(l => l.Labourer).ToList();
-            //List<Labourer> availableLabourers = labourers.Except(labourerScheduled).ToList();
-
+            List<Labourer> availableLabourers = _context.LabourerSkill.Where(ls => ls.SkillId == id).Select(ols => ols.Labourer).
+                Where(l=>l.IsAvailable == true).ToList();
             List<LabourerAssignVM> labourerAss = availableLabourers.Select(l => new LabourerAssignVM()
             {
                 labourer = l,
