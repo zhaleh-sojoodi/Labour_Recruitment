9d1869d136081c073a6ae957ded1c0f638e558b1
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-05 19:01:30 -0700

    Use Layout template

--- client/src/views/Indicents.js
+++ client/src/views/Indicents.js
@@ -1,14 +1,24 @@
 import React , {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
-import TopNav from './components/TopNav';
-import SideNav from './components/SideNav';
-import * as Auth from '../utils/Auth'
+import * as Auth from '../utils/Auth';
+import * as DataSanitizer from '../utils/DataSanitizer';
+
+import Layout from './components/Layout';
+import PageHeader from './components/PageHeader';
+import Loader from './components/Loader';
+import Table from './components/Table';
+
+import { INCIDENTS_TABLE_COLUMNS } from '../utils/TableColumns';
 
 const BASE_URL = "http://localhost:5001/api";
 
-const Incidents = () => {
-    const [incidents, setIncidents] = useState()
+const Incidents = (props) => {
+
+    const [authorized] = useState(Auth.authenticateClient() || Auth.authenticateLabourer());
+
+    const [loaded, setLoaded] = useState(false);
+    const [incidents, setIncidents] = useState();
 
     const fetchIncidents = async() => {
         let token = Auth.getToken()
@@ -26,91 +36,64 @@ const Incidents = () => {
                 throw response;
             }
             
-            let data = await response.json()
-            console.log(data)
-            setIncidents(data)
+            let data = await response.json();
+
+            if(data.length) {
+                setIncidents(DataSanitizer.cleanIncidentsData(data));
+            }
         } catch (e) {
             console.error(e);
         }
+        setLoaded(true);
     }
 
     useEffect(() => {
-        fetchIncidents()
+        fetchIncidents();
     }, [])
 
-    return (
+    const content = !authorized ? <Redirect to="/dashboard" />  :
+    (
     <>
-    {incidents && 
-    <div className="dashboard-main-wrapper">
-        <TopNav />
-        <SideNav />
+    <PageHeader
+        title={"Incidents"}
+        breadcrumbs={[
+            { name: "Home", path: "/dashboard" },
+            { name: "Incidents" }
+        ]}
+    />
 
-        <div className="dashboard-wrapper">
-        <div className="container-fluid dashboard-content">
-
-            {/* Page Header */}
-            <div className="row">
-            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-            <div className="page-header">
-                <h2 className="pageheader-title">Incident Reports</h2>
-                <div className="page-breadcrumb">
-                <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item">
-                        <Link to="/dashboard" className="breadcrumb-link">Home</Link>
-                    </li>
-                    <li className="breadcrumb-item active" aria-current="page">Incidents</li>
-                </ol>
-                </nav>
-                </div>
-            </div>
-            </div>
-            </div>
-
-            {/* Incidents */}
-            <div className="row">
-            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-            <div className="card">
-                <h5 className="card-header">Incidents</h5>
-                <div className="card-body">
-                <div className="table-responsive">
-                <Link to="/addincident" className="btn btn-primary px-3 py-2 mb-2 float-right">Add Incident</Link>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Injury</th>
-                        <th># affected</th>
-                        <th>Job</th>
-                        <th>Reports</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody> 
-                        {incidents.map((incident, i) => (
-                            <tr key={i}>
-                                <td><time>{incident.incidentReportDate.split('T')[0]}</time></td>
-                                <td>{incident.incidentType.incidentTypeName}</td>
-                                <td>{incident.labourerIncidentReport.length}</td>
-                                <td>{incident.job.title}</td>
-                                <td><span className="badge badge-danger">Required</span></td>
-                                <td><Link to={`/incident/${incident.incidentReportId}`}>View Details</Link></td>
-                            </tr>
-                         ))   
-                        }
-                    </tbody>
-                </table>
-                </div>
-                </div>
-            </div>
-            </div>
+    <div className="row">
+    <div className="col">
+    <div className="card">
+        <div className="card-header d-flex">
+            <h4 className="card-header-title">Incidents</h4>
+            <div className="toolbar ml-auto">
+                <Link
+                    to="/addincident"
+                    className="btn btn-primary btn-sm"
+                >
+                    Add New
+                </Link>
             </div>
         </div>
+        <div className="card-body">
+            <Loader loaded={loaded}>
+                <Table
+                    data={incidents}
+                    columns={INCIDENTS_TABLE_COLUMNS}
+                    path="/incident"
+                    searchable="true"
+                    {...props}
+                />
+            </Loader>
         </div>
     </div>
-    }
+    </div>
+    </div>
     </>
-    )
+    );
+
+    return <Layout content={content} />;
 }
 
 export default Incidents;
