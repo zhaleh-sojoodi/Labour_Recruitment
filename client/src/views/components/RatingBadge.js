5b2ee59d023d7cb8b116335faded2ea59a050c83
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-05 19:03:39 -0700

    Clear warnings

--- client/src/views/components/RatingBadge.js
+++ client/src/views/components/RatingBadge.js
@@ -5,9 +5,9 @@ const RatingBadge = ({rating}) => {
     const output = rating == 0 || !rating ? "N/A" : `${Math.round((rating / 5) * 100)}%`;
 
     const color = (
-        rating == 0 || !rating ? "info" :
+        rating === 0 || !rating ? "info" :
         rating >= 4 ? "success" :
-        rating == 3 ? "warning" :
+        rating === 3 ? "warning" :
         "danger"
     );
 
