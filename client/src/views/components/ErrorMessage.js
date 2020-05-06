558d040d960fffa66b44a186732d6369b66913aa
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-06 01:23:23 -0700

    Add bottom margin

--- client/src/views/components/ErrorMessage.js
+++ client/src/views/components/ErrorMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ErrorMessage = ({ message }) => {
     return (
-    <div className="alert alert-danger">
+    <div className="alert alert-danger mb-4">
         <span className="h5">{message}</span>
     </div>
     )
