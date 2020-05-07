47fd7b91c134b97f84fddfe603f32ca7d65a4a27
mingwang168 <mingwang0904@gmail.com>
2020-05-07 12:23:43 -0700

    close #282 Fix GetJobByLabourerId to return JobLabourer for just that Labourer

---
+++ backEnd/roleDemo/Controllers/GetJobLabourerByLabourerIdController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using labourRecruitment.Models.LabourRecruitment;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labourRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetJobLabourerByLabourerIdController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetJobLabourerByLabourerIdController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetLabourerByLabourerId(int id)
+        {
+            List<JobLabourer> Joblabourers = _context.JobLabourer.Where(jl => jl.LabourerId == id).ToList();
+
+            return new ObjectResult(Joblabourers);
+        }
+
+    }
+}
