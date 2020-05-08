a36bf2cd46fcd6c577b85634eb1cc65495177c78
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-08 01:06:26 -0700

    Reverse list order

--- client/src/views/Indicents.js
+++ client/src/views/Indicents.js
@@ -49,6 +49,7 @@ const Incidents = (props) => {
             
             // Process response data
             let data = await response.json();
+            
             if(data.length) {
                 let formattedData = data.map(d => ({
                     id: d.incidentReportId,
@@ -63,7 +64,7 @@ const Incidents = (props) => {
                     {Header: 'Job Title', accessor: 'job'},
                 ]);
 
-                setIncidents(formattedData);
+                setIncidents(formattedData.reverse());
             }
         } catch (e) {
             console.error(e);
