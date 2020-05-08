5bff08e07ca6ea039c1f42a002212c79f1caaa2a
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-08 08:35:42 -0700

    Install rc-tabs package

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import './assets/other/bootstrap/css/bootstrap.min.css';
 import './assets/fonts/circular-std/style.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import 'rc-tabs/assets/index.css';
 import './assets/css/style.css';
 
 ReactDOM.render(
