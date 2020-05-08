fd0f72ca63b2972f3fe8ec2357bb51843de4ebb1
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-08 13:49:04 -0700

    Remove stats

--- client/src/views/client/ClientDashboard.js
+++ client/src/views/client/ClientDashboard.js
@@ -80,64 +80,6 @@ const ClientDashboard = (props) => {
         { !jobs ?
         <ErrorMessage message={"You have not created any jobs yet."} /> :
         <>
-        {/* Stats */}
-        <div className="row">
-            <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-	        <div className="card">
-	        <div className="card-body">
-                <div className="d-inline-block">
-                    <h5 className="text-muted">Active Jobs</h5>
-                    <h2 className="mb-0">5</h2>
-                </div>
-                <div className="float-right icon-circle-medium icon-box-lg bg-info-light mt-1">
-                    <i className="material-icons text-info">work</i>
-                </div>
-	        </div>
-	        </div>
-	        </div>
-
-            <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-	        <div className="card">
-	        <div className="card-body">
-                <div className="d-inline-block">
-                    <h5 className="text-muted">Labourers Hired</h5>
-                    <h2 className="mb-0">295</h2>
-                </div>
-                <div className="float-right icon-circle-medium icon-box-lg bg-success-light mt-1">
-                    <i className="material-icons text-success">accessibility</i>
-                </div>
-	        </div>
-	        </div>
-	        </div>
-
-            <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-	        <div className="card">
-	        <div className="card-body">
-                <div className="d-inline-block">
-                    <h5 className="text-muted">Hiring Costs</h5>
-                    <h2 className="mb-0">$24,800</h2>
-                </div>
-                <div className="float-right icon-circle-medium icon-box-lg bg-secondary-light mt-1">
-                    <i className="material-icons text-secondary">monetization_on</i>
-                </div>
-	        </div>
-	        </div>
-	        </div>
-
-            <div className="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-	        <div className="card">
-	        <div className="card-body">
-                <div className="d-inline-block">
-                    <h5 className="text-muted">Company Rating</h5>
-                    <h2 className="mb-0">75.4%</h2>
-                </div>
-                <div className="float-right icon-circle-medium icon-box-lg bg-brand-light mt-1">
-                    <i className="material-icons text-brand">thumb_up</i>
-                </div>
-	        </div>
-	        </div>
-	        </div>
-        </div>
 
         {/* Jobs Table */}
         <div className="row">
