f295be1d2554e40bd2cd650a49cf658abe20d4cc
Nicole Ling <47255597+nicolesyling@users.noreply.github.com>
2020-05-08 10:00:38 -0700

    Modify to generate invoices

--- client/src/views/client/ClientInvoices.js
+++ client/src/views/client/ClientInvoices.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 import * as Auth from '../../utils/Auth';
+import * as DataSanitizer from '../../utils/DataSanitizer';
 
 import Loader from '../components/Loader';
 import Layout from '../components/Layout';
 import PageHeader from '../components/PageHeader';
-import Table from '../components/Table';
+import Select from 'react-select';
+import FormErrors from '../components/FormErrors';
 import ErrorMessage from '../components/ErrorMessage';
 import UnauthorizedMessage from '../components/UnauthorizedMessage';
 
@@ -18,10 +20,16 @@ const ClientInvoices = (props) => {
 
     // Components
     const [loaded, setLoaded] = useState(false);
+    const [formErrors, setFormErrors] = useState([]);
 
     // Data
     const [jobs, setJobs] = useState();
     const [jobsTableColumns, setJobsTableColumns] = useState();
+    const [weeks, setWeeks] = useState();
+
+    // Form
+    const [selectedJob, setSelectedJob] = useState();
+    const [selectedWeek, setSelectedWeek] = useState();
 
     const fetchJobs = async() => {
         try {
@@ -40,28 +48,101 @@ const ClientInvoices = (props) => {
             let data = await response.json();
     
             if(data.length) {
-                let formattedData = data.map(d => ({
-                    id: d.jobId,
-                    title: d.title,
-                    status: d.isComplete ? "Complete" : "In Progress"
+                setJobs(data.map((d) => {
+                    return {
+                        label: d.title,
+                        value: d.jobId
+                    }
                 }));
-    
-                setJobs(formattedData);
-                setJobsTableColumns([
-                    {Header: 'Job Title', accessor: 'title'},
-                    {Header: 'Completion Status', accessor: 'status'},
-                ]);
             }
         } catch(e) {
             console.error(e);
         }
+    }
 
-        // Set loading state
-        setLoaded(true);
+    const fetchInvoiceWeeksByJobID = async(id) => {
+        try {
+            const URI = BASE_URL + `/ClientInvoice/${id}`;
+            let response = await fetch(URI, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${Auth.getToken()}`
+                }
+            });
+    
+            if(response.status !== 200) {
+                throw response;
+            }
+    
+            let data = await response.json();
+
+            if(data.length) {
+                setWeeks(data.map((d) => {
+                    let start = DataSanitizer.formatDateString(d.firstDay);
+                    let end = DataSanitizer.formatDateString(d.lastDay);
+                    let label = `${start} - ${end}`;
+                    return {
+                        label: label,
+                        value: "",
+                        JobId: d.jobId,
+                        StartDay: DataSanitizer.formatDateParams(d.firstDay),
+                        EndDay: DataSanitizer.formatDateParams(d.lastDay)
+                    }
+                }));
+            } else {
+                setSelectedWeek(null);
+            }
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    const onChangeJob = (selection) => {
+        if(selection) {
+            setSelectedJob(selection.value);
+            fetchInvoiceWeeksByJobID(selection.value);
+        }
+    }
+
+    const onChangeWeek = (selection) => {
+        if(selection) {
+            setSelectedWeek(selection);
+        }
+    }
+
+    const validateForm = _ => {
+        let errors = [];
+
+        if(!selectedJob) {
+            errors.push("No job has been selected.");
+        }
+
+        if(!selectedWeek) {
+            errors.push("No billing week has been selected.");
+        }
+
+        if(errors.length) {
+            setFormErrors(errors);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+
+        if(validateForm()) {
+            const { JobId, StartDay, EndDay } = selectedWeek;
+            props.history.push(`/invoice/${JobId}/${StartDay}/${EndDay}`);
+        }
     }
 
     useEffect(() => {
-        if(authorized) fetchJobs();
+        if(authorized) {
+            fetchJobs();
+            setLoaded(true);
+        }
     }, [])
 
     const content = (
@@ -78,17 +159,40 @@ const ClientInvoices = (props) => {
         <div className="row">
         <div className="col">
         <div className="card">
-        <h5 className="card-header">Invoices</h5>
+        <h5 className="card-header">Generate an Invoice</h5>
         <div className="card-body">
-            { !jobs ? <ErrorMessage message={"No invoices to display."} /> :
-            <Table
-                data={jobs}
-                columns={jobsTableColumns}
-                path="/invoice"
-                searchable={true}
-                {...props}
+        
+        {formErrors.length > 0 && (
+        <FormErrors errors={formErrors} />
+        )}
+
+        <form onSubmit={submitForm}>
+            <label htmlFor="job">Select Job</label>
+            <Select
+                required
+                name="job"
+                options={jobs}
+                onChange={onChangeJob}
+                className="mb-3"
             />
-            }
+
+            <label htmlFor="job">Select Billing Week</label>
+            <Select
+                required
+                value={selectedWeek}
+                name="week"
+                options={weeks}
+                onChange={onChangeWeek}
+                isDisabled={selectedJob ? false : true}
+                className="mb-3"
+            />
+
+            <button
+                className="btn btn-primary btn-lg mt-3 float-right"
+            >
+                Generate
+            </button>
+        </form>
         </div>
         </div>
         </div>
