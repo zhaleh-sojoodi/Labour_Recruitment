f6061ddec1263d86912e1d062e577a7d843c6755
Jaleh Sojoodi <jaleh36@gmail.com>
2020-05-08 03:58:48 -0700

    Cleaned Code

--- backEnd/roleDemo/Controllers/HomeController.cs
+++ backEnd/roleDemo/Controllers/HomeController.cs
@@ -12,12 +12,10 @@ using roleDemo.Models;
 namespace roleDemo.Controllers {
     public class HomeController : Controller {
 
-        private readonly EmailSettings _emailSettings;
-        private readonly ApplicationDbContext _context;
-        public HomeController(IOptions<EmailSettings> emailSettings, ApplicationDbContext context)
+      
+        public HomeController()
         {
-            _emailSettings = emailSettings.Value;
-            _context = context;
+            
         }
 
         public IActionResult Index() {
@@ -33,27 +31,7 @@ namespace roleDemo.Controllers {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        //public IActionResult Contact()
-        //{
-        //    //EmailContent emailContent = new EmailContent
-        //    //{
-        //    //    LabourerFirstName = "Ming",
-        //    //    LabourerLastName = "Wang",
-        //    //    JobAddress = "BCIT downtown campus",
-        //    //    JobStart = Convert.ToDateTime("2020-05-15 00:00:00"),
-        //    //    JobEnd = Convert.ToDateTime("2020-05-20 00:00:00")
-        //    //};
-        //    //if (new EmailHelper(_emailSettings).SendMail("minghuiwang0904@gmail.com", "Your Job Schedule", emailContent))
-        //    //    return RedirectToAction("Index");
-        //    //return RedirectToAction("Error");
-        //}
-
-        //public IActionResult CheckCompletion()
-        //{
-        //    new CheckCompletion(_context).CheckIfComplete();
-        //        return RedirectToAction("Index");
-
-        //}
+       
 
     }
 }
